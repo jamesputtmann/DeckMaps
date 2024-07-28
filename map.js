877f3fd15f38a7c9ab47c57cc6acffa7c88f5841
James Puttmann <james@vanwestpartners.com>
2024-07-28 15:38:19 -0600

    Async Map Loading

--- map.js
+++ map.js
@@ -1,23 +1,28 @@
 function loadMap() {
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+    const script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=marker&v=beta';
+    script.async = true;
     document.head.appendChild(script);
 }
 
 function initMap() {
-    var center = { lat: 34.00493516456416, lng: -83.92118813558204 };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    const center = { lat: 34.00493516456416, lng: -83.92118813558204 };
+    const map = new google.maps.Map(document.getElementById('map'), {
         center: center,
         zoom: 12
     });
-    var marker = new google.maps.Marker({
+
+    const markerOptions = {
         position: center,
         map: map,
         title: 'Center Location'
-    });
-    var radius3Miles = 3 * 1609.34;
-    var radius5Miles = 5 * 1609.34;
-    var circle3Miles = new google.maps.Circle({
+    };
+    const marker = new google.maps.marker.AdvancedMarkerElement(markerOptions);
+
+    const radius3Miles = 3 * 1609.34;
+    const radius5Miles = 5 * 1609.34;
+
+    const circle3Miles = new google.maps.Circle({
         map: map,
         center: center,
         radius: radius3Miles,
@@ -27,7 +32,8 @@ function initMap() {
         strokeOpacity: 0.5,
         strokeWeight: 2
     });
-    var circle5Miles = new google.maps.Circle({
+
+    const circle5Miles = new google.maps.Circle({
         map: map,
         center: center,
         radius: radius5Miles,
